e629c36147f75e1c722a869db78484ba16823adb
bertyn99 <bboulikou@gmail.com>
2025-07-02 11:07:38 +0200

    fix

--- config/plugins.ts
+++ config/plugins.ts
@@ -23,9 +23,6 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  calendar: {
-    enabled: true,
-  },
   "local-image-sharp": {
     config: {
       maxAge: 31536000, // which corresponds to 1 year: 60 seconds × 60 minutes × 24 hours × 365 days = 31536000 seconds.
